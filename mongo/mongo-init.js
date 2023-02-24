954d9cbf934a70d3ad581916497cf1fb63269f7b
komtriangle <komlevdanila742@gmail.com>
2023-02-24 16:21:53 +0300

    mongo docker

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -1,3 +1,11 @@
+db.createUser(
+    {
+        user: "films_user",
+        pwd: "films_password",
+        roles: ["root"]
+    }
+);
+
 db = db.getSiblingDB('films_db');
 db.createCollection('films');
 db.createCollection('actors');
